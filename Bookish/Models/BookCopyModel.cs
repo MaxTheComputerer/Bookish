5365f0fea9f5d6319b23d2ff423959d2979d72a7
Max Johnson <max.johnson@softwire.com>
2022-07-12 14:27:11 +0100

    Add models and tables for copies of books and library members

---
+++ Bookish/Models/BookCopyModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookish.Models;
+
+public class BookCopyModel
+{
+    public int Id { get; set; }
+    public BookModel Book { get; set; }
+    public MemberModel? Borrower { get; set; }
+    public DateTime DueDate { get; set; }
+
+    public bool IsAvailable()
+    {
+        return Borrower == null;
+    }
+
+    public bool IsOverdue()
+    {
+        return DueDate.Date < DateTime.Today.Date;
+    }
+}
