6daf373457f5bbd7a6e7a0eff151582471e2ead8
Max Johnson <max.johnson@softwire.com>
2022-07-14 11:23:15 +0100

    Move book copy actions into Book controller

--- Bookish/Controllers/HomeController.cs
+++ Bookish/Controllers/HomeController.cs
@@ -18,51 +18,6 @@ public class HomeController : Controller
         return View();
     }
 
-    [HttpGet]
-    public IActionResult ViewCopies(BookModel book)
-    {
-        if (book == null || book.Id == null)
-        {
-            return RedirectToAction(nameof(Catalogue));
-        }
-        var result = BookCopyService.GetCopies(book);
-        return View(result);
-    }
-    
-    [HttpPost]
-    public ActionResult AddCopy(BookModel book)
-    {
-        BookCopyService.InsertBookCopy(book.Id);
-        return RedirectToAction(nameof(ViewCopies), new { Id = book.Id });
-    }
-    
-    [HttpPost]
-    public ActionResult DeleteCopy(BookCopyModel copy)
-    {
-        var book = BookCopyService.GetBookFromCopy(copy.Id);
-        BookCopyService.DeleteCopy(copy.Id);
-        return RedirectToAction(nameof(ViewCopies), new { Id = book.Id });
-    }
-    
-    [HttpPost]
-    public ActionResult CheckInCopy(BookCopyModel copy)
-    {
-        BookCopyService.CheckInCopy(copy.Id);
-        var book = BookCopyService.GetBookFromCopy(copy.Id);
-        return RedirectToAction(nameof(ViewCopies), new { Id = book.Id });
-    }
-    
-    [HttpPost]
-    public ActionResult CheckOutCopy(CheckOutCopyModel checkOutModel)
-    {
-        using var context = new LibraryContext();
-        var member = context.Members.Find(checkOutModel.BorrowerId);
-
-        BookCopyService.CheckOutCopy(checkOutModel.Id, member, checkOutModel.DueDate);
-        var book = BookCopyService.GetBookFromCopy(checkOutModel.Id);
-        return RedirectToAction(nameof(ViewCopies), new { Id = book.Id });
-    }
-
     public IActionResult Privacy()
     {
         return View();
