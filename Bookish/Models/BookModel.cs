8806b5a623f3d4dceaa706df758f33cbdc504a9a
Claire Carlotti <Claire.Calotti@softwire.com>
2022-07-15 12:43:40 +0100

    All properties of Book and Member made nullable

--- Bookish/Models/BookModel.cs
+++ Bookish/Models/BookModel.cs
@@ -5,7 +5,7 @@ public class BookModel
     public int Id { get; set; }
     public string? Author { get; set; }
     public string? Title { get; set; }
-    public int Publication_Year { get; set; }
+    public int? Publication_Year { get; set; }
     public string? ISBN { get; set; }
     public string? Genre { get; set; }
 
--- Bookish/Models/MemberModel.cs
+++ Bookish/Models/MemberModel.cs
@@ -3,8 +3,8 @@
 public class MemberModel
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public DateTime DoB { get; set; }
-    public string Email { get; set; }
-    public string Mobile { get; set; }
+    public string? Name { get; set; }
+    public DateTime? DoB { get; set; }
+    public string? Email { get; set; }
+    public string? Mobile { get; set; }
 }
